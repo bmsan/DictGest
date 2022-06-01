c4b079e433220fdf0355ccb68cab63740bd418f8
bmsan <bmsanbmsan@outlook.com>
2022-06-01 08:45:11 +0200

    Updated package distribution

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.9",
     install_requires=requirements,
     url="https://github.com/bmsan/DictGest",
 )
